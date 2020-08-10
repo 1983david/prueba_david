282d74c59b425a5cea8ad9dcd3a9e4dcdf8c4ffb
deivid <davidguiahurtado2@gmail.com>
2020-08-10 07:54:08 -0500

    arquitectura mvc listo por david

--- src/java/Modelo/Estudiante.java
+++ src/java/Modelo/Estudiante.java
@@ -10,5 +10,95 @@ package Modelo;
  * @author TuNombre
  */
 public class Estudiante {
-    
+    private int idestudiante ;
+    private String nombre ;
+    private String apellido;
+    private String dni;
+    private String codigo;
+    private String estado;
+
+    /**
+     * @return the idestudiante
+     */
+    public int getIdestudiante() {
+        return idestudiante;
+    }
+
+    /**
+     * @param idestudiante the idestudiante to set
+     */
+    public void setIdestudiante(int idestudiante) {
+        this.idestudiante = idestudiante;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the dni
+     */
+    public String getDni() {
+        return dni;
+    }
+
+    /**
+     * @param dni the dni to set
+     */
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    /**
+     * @return the codigo
+     */
+    public String getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
+
